e4f2e2f36223b1de4cdb214880132d5406ada57c
Sanskar <thakursanskaranhal@gmail.com>
2022-09-17 21:14:02 +0530

    added animations to the app

--- app/src/main/java/com/stop_watch/MainActivity.kt
+++ app/src/main/java/com/stop_watch/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.stop_watch
 
 import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -14,30 +17,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val constraintLayout: ConstraintLayout = findViewById<ConstraintLayout>(R.id.mainLayout)
-        val animationDrawable : AnimationDrawable = constraintLayout.background as AnimationDrawable
-        animationDrawable.setEnterFadeDuration(2500)
-        animationDrawable.setExitFadeDuration(2500)
-        animationDrawable.start()
-
         val startbtn = findViewById<Button>(R.id.startbtn)
         val pausebtn = findViewById<Button>(R.id.pausebtn)
         val resetbtn = findViewById<Button>(R.id.resetbtn)
         val stopWatch = findViewById<Chronometer>(R.id.stopWatch)
         var stopAt:Long=0
+        val black :Drawable 
 
         startbtn.setOnClickListener {
+            //transition.startTransition(500)
+            val constraintLayout :ConstraintLayout= findViewById<ConstraintLayout>(R.id.mainLayout)
+            constraintLayout.setBackgroundResource(R.drawable.gradient_list)
+            var animation = constraintLayout.background as AnimationDrawable
+            animation.setEnterFadeDuration(2500)
+            animation.setExitFadeDuration(2500)
+            animation.start()
+
             stopWatch.base= SystemClock.elapsedRealtime()-stopAt
             stopWatch.start()
         }
 
 
         pausebtn.setOnClickListener {
+            val constraintLayout :ConstraintLayout= findViewById<ConstraintLayout>(R.id.mainLayout)
+            constraintLayout.setBackgroundResource(R.drawable.gradient_list)
+            var animation = constraintLayout.background as AnimationDrawable
+            animation.stop()
             stopAt=SystemClock.elapsedRealtime()-stopWatch.base
             stopWatch.stop()
         }
 
         resetbtn.setOnClickListener {
+            val constraintLayout :ConstraintLayout= findViewById<ConstraintLayout>(R.id.mainLayout)
+            constraintLayout.setBackgroundResource(R.drawable.transition)
+            var transition = constraintLayout.background as TransitionDrawable
+            transition.reverseTransition(500)
+          //  animationDrawable.stop()
             stopWatch.base=SystemClock.elapsedRealtime()
             stopWatch.stop()
             stopAt=0
